4ea357db066095a0b9ae8ea4526fd8c2bd67bdb3
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-12 22:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 // Code your solutions in this file
 // Code your solutions in this file
 // var namesArray = ["Ada", "Brendan", "Ali"];
+
 function printBadges(names){
-  for (let i = 0; i < names.length; i++){
-    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
-  }
-  return names
+ for (let i = 0; i < names.length; i++){
+   console.log(`Welcome ${names[i]}! You are employee #${i+1}.`);
+ }
+ return names
 }
 // printBadges(namesArray)
-// var tails = 0
+
+// let tails = 0
 function tailsNeverFails(){
-  let tails = 0
-  var number = Math.random()
-  while (number <= 0.5){
-    ++tails
-    number = Math.random()
-  }
-  return `You got ${tails} tails in a row!`
+ let tails = 0
+ let number = Math.random()
+ while (number >= 0.5){
+   ++tails
+   number = Math.random()
+ }
+ return `You got ${tails} tails in a row!`
 }
